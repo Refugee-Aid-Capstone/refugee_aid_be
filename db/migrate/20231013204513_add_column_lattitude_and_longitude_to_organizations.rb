8b6c8f189f62c9e21becc48b629ad7de0f3c6c56
Davis Weimer <davisweimer@gmail.com>
2023-10-13 17:37:23 -0600

    fix: Removed extra migration file

--- db/migrate/20231013204513_add_column_lattitude_and_longitude_to_organizations.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnLattitudeAndLongitudeToOrganizations < ActiveRecord::Migration[7.0]
-  def change
-    add_column :organizations, :latitude, :float
-    add_column :organizations, :longitude, :float
-  end
-end
