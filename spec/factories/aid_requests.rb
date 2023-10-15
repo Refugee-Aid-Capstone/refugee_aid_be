88575f9bfa539bcc0a879d2be88e61c41acd1a67
Davis Weimer <davisweimer@gmail.com>
2023-10-15 13:23:40 -0600

    fix: Ruby version updated

--- spec/factories/aid_requests.rb
+++ spec/factories/aid_requests.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :aid_request do
     organization
-    aid_type { "language" }
+    aid_type { AidRequest.aid_types.keys.sample.to_sym }
     language { "Arabic" }
     description { "MyText" }
-    status { "active" }
+    status { AidRequest.statuses.keys.sample.to_sym }
   end
 end
