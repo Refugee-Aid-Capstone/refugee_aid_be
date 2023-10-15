ea57030a3e8b8f0daf8b3d86cc89e9e24d3f7ffb
Artemy Gibson <artemygibson@gmail.com>
2023-10-14 17:54:39 -0700

    feat: Add enums for aid_type attribute in AidRequest model

--- app/models/aid_request.rb
+++ app/models/aid_request.rb
@@ -2,5 +2,6 @@ class AidRequest < ApplicationRecord
   belongs_to :organization
   validates :aid_type, :description, :status, presence: true
 
+  enum aid_type: [:medical, :language, :food, :legal, :other]
   enum status: [:active, :pending, :fulfilled]
 end
