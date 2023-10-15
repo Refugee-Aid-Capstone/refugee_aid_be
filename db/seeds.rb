f81cefce716d3b6023b0be7803d2f73dacf3d019
Davis Weimer <davisweimer@gmail.com>
2023-10-15 15:08:58 -0600

    feat: CSV db seeding intialized

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
+require 'csv'
 
-# AidRequest.destroy_all
-# Organization.destroy_all
+AidRequest.destroy_all
+Organization.destroy_all
 
 # KEEP FOR NOW - Used to grab all lat/lon for cities above, need to figure out memoizing or caching perhaps
 # Geocoder will freeze in current config 🤬
@@ -13,20 +14,42 @@ coordinates_by_city_state = Hash.new
 # end
 =end
 
+CSV.foreach(Rails.root.join('db/data/organizations_gen.csv'), headers: true) do |row|
+  Organization.create(
+    id: row["id"],
+    name: row["name"],
+    contact_phone: row["contact_phone"],
+    contact_email: row["contact_email"],
+    street_address: row["street_address"],
+    website: row["website"],
+    city: row["city"],
+    state: row["state"],
+    zip: row["zip"],
+    latitude: row["latitude"],
+    longitude: row["longitude"],
+    share_address: row["share_address"],
+    share_phone: row["share_phone"],
+    share_email: row["share_email"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  )
+  ActiveRecord::Base.connection.reset_pk_sequence!('organizations')
+end
 
+puts "Organizations CSV loaded successfully."
 
-# 1000.times do
-#   city, state = cities_and_states.sample
-#   name = refugee_shelter_names.sample
-#   coordinates = coordinates_by_city_state["#{city}, #{state}"]
-#   random_lat = rand(lat_min_range..lat_max_range) + coordinates[0]
-#   random_lon = rand(long_min_range..long_max_range) + coordinates[1]
-#   FactoryBot.create(:organization, name: name, city: city, state: state, latitude: random_lat, longitude: random_lon)
-# end
+CSV.foreach(Rails.root.join('db/data/aid_requests_gen.csv'), headers: true) do |row|
+  AidRequest.create(
+    id: row["id"],
+    organization_id: row["organization_id"],
+    aid_type: row["aid_type"],
+    language: row["language"],
+    description: row["description"],
+    status: row["status"],
+    created_at: row["created_at"],
+    updated_at: row["updated_at"]
+  )
+  ActiveRecord::Base.connection.reset_pk_sequence!('aid_requests')
+end
 
-# ids = Organization.all.pluck(:id)
-
-# 3000.times do 
-#   FactoryBot.create(:aid_request, organization_id: ids.sample)
-# end
-
+puts "Aid Requests CSV loaded successfully."
