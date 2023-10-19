391c78cf2e8d66be543adfcca1121f6d2fc0bc88
Artemy Gibson <artemygibson@gmail.com>
2023-10-19 13:12:31 -0700

    refactor: add custom validations for contact info to organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,13 +2,13 @@ class Organization < ApplicationRecord
   has_many :aid_requests
 
   validates :name, 
-            :contact_phone, 
-            :contact_email, 
-            :street_address,
-            :website,
+            # :contact_phone, should be optional
+            # :contact_email, should be optional
+            # :street_address, should be optional
+            # :website, #should be optional
             :city,
             :state,
-            :zip,
+            :zip,  #make city and state optional, and use zip/geocoder to fetch city and state when not provided?
             :latitude,
             :longitude,
             presence: :true
@@ -17,4 +17,20 @@ class Organization < ApplicationRecord
             :share_phone,
             :share_email,
             inclusion: [true,false]
+
+  validate :contact_info_given
+
+  validate :contact_info_shared
+
+  def contact_info_given
+    unless contact_phone || contact_email || street_address
+      errors.add(:please_include, "either a phone number, email address, and/or street address")
+    end
+  end
+
+  def contact_info_shared
+    unless share_address || share_phone || share_email
+      errors.add(:please_share, "at least one form of contact so volunteers may get in touch with you.")
+    end
+  end
 end
