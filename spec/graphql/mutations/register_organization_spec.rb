d9f32767d5632266a8ea19b15e62ec9ada785b15
Artemy Gibson <artemygibson@gmail.com>
2023-10-19 13:13:27 -0700

    test: add skeleton tests for RegisterOrganization mutation

---
+++ spec/graphql/mutations/register_organization_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe RegisterOrganization do
+  xit "creates a new organization when given correct params" do
+
+  end
+
+  xit "does not create a new organization if missing address info" do
+
+  end
+end
