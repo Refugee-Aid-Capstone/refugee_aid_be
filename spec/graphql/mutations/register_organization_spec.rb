5a26b51b4c5692495054cb3fa4e29f3ca0865c8b
Artemy Gibson <artemygibson@gmail.com>
2023-10-21 19:31:52 -0700

    test: Add query test

--- spec/graphql/mutations/register_organization_spec.rb
+++ spec/graphql/mutations/register_organization_spec.rb
@@ -2,7 +2,27 @@ require "rails_helper"
 
 RSpec.describe RegisterOrganization do
   xit "creates a new organization when given correct params" do
-
+    query = <<-GRAPHQL
+    query createOrg($id: ID!){
+      organization(id: $id) {
+        id
+        name
+        contactPhone
+        contactEmail
+        streetAddress
+        website
+        city
+        state
+        zip
+        latitude
+        longitude
+        shareAddress
+        sharePhone
+        shareEmail
+        }
+      }
+    }
+    GRAPHQL
   end
 
   xit "does not create a new organization if missing address info" do
