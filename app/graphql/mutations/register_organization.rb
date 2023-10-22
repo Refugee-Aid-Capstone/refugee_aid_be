a79cc9b50445fbfb765a649a11105dafc9557d68
Artemy Gibson <artemygibson@gmail.com>
2023-10-22 12:02:18 -0700

    feat: Write resolve method to create an Organization

--- app/graphql/mutations/register_organization.rb
+++ app/graphql/mutations/register_organization.rb
@@ -9,25 +9,29 @@ module Mutations
     argument :name, String, required: true
     argument :contact_phone, String, required: false
     argument :contact_email, String, required: false
-    argument :street_address, String, required: true
+    argument :street_address, String, required: false
     argument :website, String, required: false
     argument :city, String, required: true
     argument :state, String, required: true
     argument :zip, String, required: true
-    # argument :latitude, Float, required: false
-    # argument :longitude, Float, required: false
     argument :share_address, Boolean, required: false
     argument :share_phone, Boolean, required: false
     argument :share_email, Boolean, required: false
 
 
-    def resolve(name:, contact_phone:, contact_email:, 
-                street_address:, website:, city:, 
-                state:, zip:, share_address:, share_phone:, share_email:)
-      # latitude = contact geocoder api, use given address
-      # longitude = contact geocoder api, use given address
-      
-      Organization.create!(name: name, contact_phone: contact_phone, contact_email: contact_email, street_address: street_address, website: website, city: city, state: state, zip: zip, latitude: latitude, longitude: longitude, share_address: share_address, share_phone: share_phone, share_email: share_email)
+    def resolve(**args)
+      location = Geocoder.search(address(args))
+      args[:latitude] = location.first.latitude
+      args[:longitude] = location.first.longitude
+      begin
+        Organization.create!(args)
+      rescue ActiveRecord::RecordInvalid => error
+        raise GraphQL::ExecutionError, "#{error.message}"
+      end
+    end
+
+    def address(args)
+      [args[:street_address], args[:city], args[:state], args[:zip]].compact.join(', ')
     end
   end
 end
