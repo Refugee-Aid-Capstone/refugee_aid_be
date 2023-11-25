b00eba292d12811c89069594a929db590ff53ecf
Artemy Gibson <artemygibson@gmail.com>
2023-11-25 15:33:47 -0700

    Set up Volunteer type and graphql volunteer type test

--- app/graphql/types/volunteer_type.rb
+++ app/graphql/types/volunteer_type.rb
@@ -2,5 +2,24 @@
 
 module Types
   class VolunteerType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String
+    field :last_name, String
+    field :email, String
+    # field :street_address, String
+    # field :website, String
+    # field :city, String
+    # field :state, String
+    # field :zip, String
+    # field :latitude, Float
+    # field :longitude, Float
+    # field :share_address, Boolean
+    # field :share_phone, Boolean
+    # field :share_email, Boolean
+    # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    # field :aid_requests, [Types::AidRequestType], null: false
+
   end
 end
