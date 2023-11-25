347b4b272f1b5ffd29a939fb9407a189bb8baee4
Artemy Gibson <artemygibson@gmail.com>
2023-11-25 15:59:02 -0700

    Remove reference comments

--- app/graphql/types/volunteer_type.rb
+++ app/graphql/types/volunteer_type.rb
@@ -6,20 +6,5 @@ module Types
     field :first_name, String
     field :last_name, String
     field :email, String
-    # field :street_address, String
-    # field :website, String
-    # field :city, String
-    # field :state, String
-    # field :zip, String
-    # field :latitude, Float
-    # field :longitude, Float
-    # field :share_address, Boolean
-    # field :share_phone, Boolean
-    # field :share_email, Boolean
-    # field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    # field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-
-    # field :aid_requests, [Types::AidRequestType], null: false
-
   end
 end
