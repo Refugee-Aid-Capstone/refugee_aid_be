dd3d86b7290048d78e58d95e53e7694a357f3c4b
Artemy Gibson <artemygibson@gmail.com>
2023-11-25 15:26:04 -0700

    Generate volunteer type, model, and model spec file

---
+++ app/graphql/types/volunteer_type.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Types
+  class VolunteerType < Types::BaseObject
+  end
+end
---
+++ app/models/volunteer.rb
@@ -0,0 +1,5 @@
+class Volunteer < ApplicationRecord
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email
+end
