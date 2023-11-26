1e917e1b06d4b2e05e54c01a924b803c9608e261
Artemy Gibson <artemygibson@gmail.com>
2023-11-26 13:42:38 -0700

    add create_aid_request mutation to mutation_type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :update_aid_request, mutation: Mutations::UpdateAidRequest
+    field :create_aid_request, mutation: Mutations::CreateAidRequest
   end
 end
