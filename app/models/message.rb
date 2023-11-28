010fc268b3b8c30e53495ca2ac5f706b966ec7ca
Artemy Gibson <artemygibson@gmail.com>
2023-11-28 11:41:00 -0800

    fix: remove unneeded methods and tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,20 +4,4 @@ class Message < ApplicationRecord
 
   enum sender: [:volunteer, :organization]
   validates_presence_of :message_body, :sender
-
-  def sent_by 
-    if sender == "organization"
-      organization
-    elsif sender == "volunteer"
-      volunteer
-    end
-  end
-
-  def recipient
-    if sender == "organization"
-      volunteer
-    elsif sender == "volunteer"
-      organization
-    end
-  end
 end
