5130745d3e21b1d60eb60ad166ca2ce52ef987b8
Artemy Gibson <artemygibson@gmail.com>
2023-11-28 11:39:07 -0800

    feat: Create mutation to send (create) a new message

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :send_message, mutation: Mutations::SendMessage
     field :update_aid_request, mutation: Mutations::UpdateAidRequest
     field :create_aid_request, mutation: Mutations::CreateAidRequest
     field :register_organization, mutation: Mutations::RegisterOrganization
